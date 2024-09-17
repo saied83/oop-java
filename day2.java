52996d259850ba52ffa346f2ebd983a93f000d1d
saied83 <saiedtechit@gmail.com>
2024-09-18 02:35:56 +0600

    scanner

---
+++ day2.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class day2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        boolean isTrue = sc.nextBoolean();
+        double measuere = sc.nextDouble();
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(measuere);
+        System.out.println(isTrue);
+    }
+}
